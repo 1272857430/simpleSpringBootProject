57a3e892bbaebe97f67d01994d25c8f968e84d85
程佳德 <>
2019-10-25 17:13:48 +0800

    up

--- 03_start_container.sh
+++ 03_start_container.sh
@@ -1,5 +1,5 @@
 docker run -d --log-opt max-size=1g --log-opt max-file=3 \
 	--name simple-server \
-	-p 80:80 \
+	-p 81:81 \
 	-v /data/logs/simple-server:/data/logs/simple-server \
 	simple-server
