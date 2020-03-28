9ca0e8c78885e54edf4453d0464a3fe4d0c6711d
程佳德 <>
2020-03-28 15:51:57 +0800

    upadte

--- src/main/java/cn/cjd/springboot/Application.java
+++ src/main/java/cn/cjd/springboot/Application.java
@@ -1,8 +1,11 @@
 package cn.cjd.springboot;
 
+import cn.cjd.springboot.modal.common.utils.springContext.SpringContextUtils;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,6 +22,13 @@ public class Application {
     public static void main( String[] args ){
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.run(args);
+    }
+
+    @Bean("defaultStart")
+    public CommandLineRunner defaultStart() {
+        return (String... strings) -> {
+            System.out.println("项目启动完毕，初始化执行的方法");
+        };
     }
 }
 
